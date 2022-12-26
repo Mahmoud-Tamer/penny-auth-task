78b73cd72e673d69073298527298eb75612ac1cf
Mahmoud-Tamer <mahmoudaltamer615@gmail.com>
2022-12-26 22:16:17 +0300

    initial push

---
+++ apps/penny-task/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SharedModule } from './shared/shared.module';
+import { HttpIntercept } from './core/interceptors/http-intercept';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store/reducers';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpIntercept,
+      multi: true,
+    },
+    MatSnackBar,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
